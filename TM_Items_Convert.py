4f4733c2322f04d976b7030ed810ce513ec84d13
skyslide <sir.von-niklas@hotmail.de>
2021-05-04 15:53:36 +0200

    better debug infos

--- TM_Items_Convert.py
+++ TM_Items_Convert.py
@@ -10,18 +10,20 @@ from .TM_Functions import *
 
 class CONVERT_ITEM(Thread):
     
-    def __init__(self, fbxfilepath: str) -> None:
+    def __init__(self, fbxfilepath: str, game: str) -> None:
         super(CONVERT_ITEM, self).__init__() #need to call init from Thread, otherwise error
         self.fbxfilepath    = fixSlash( fbxfilepath ) 
         self.fbxfilepathREL = "Items/" + fbxfilepath.split("/Work/Items/")[-1] 
         self.convertIsDone  = False
         self.convertFailed  = False
-        self.meshInfos = None   #error or succes msg of mesh/shape.gbx
-        self.itemInfos = None   #error or succes msg of item.gbx
+        self.meshInfos = None   #error or success msg of mesh/shape.gbx
+        self.itemInfos = None   #error or success msg of item.gbx
         self.itemRcode = 0      #0 good, 1 and above bad
         self.meshRcode = 0      #0 good, 1 and above bad
         self.name      = self.fbxfilepath.split("/")[-1]
+        self.nameRAW   = self.name.replace(".fbx", "")
         self.progress  = []
+        self.game      = game
 
         self.meshparamsXMLFilepath  = self.fbxfilepath.replace(".fbx", ".MeshParams.xml")
         self.itemXMLFilepath        = self.fbxfilepath.replace(".fbx", ".Item.xml")
@@ -37,6 +39,9 @@ class CONVERT_ITEM(Thread):
     
     def run(self) -> None:
         """method called when method start() is called on an instance of this class"""
+
+        self.progress.append(f"""Start convert of <{self.fbxfilepath}> for {self.game}""")
+
         if isGameTypeManiaPlanet():
             self.convertMeshAndShapeGBX()
             if not self.convertFailed:    self.hackShapeGBX(action="MAKE_OLD") #rename shape.gbx += .old
@@ -45,10 +50,9 @@ class CONVERT_ITEM(Thread):
             if not self.convertFailed:    self.hackShapeGBX(action="USE_OLD") #delte current shape.gbx, rename shape.gbx.old (-old)
             
         if not self.convertFailed:    self.convertItemGBX() #tm2020 only needs one convert
-        if not self.convertFailed:    self.progress.append("convert success")
+        if not self.convertFailed:    self.progress.append(f"Convert of <{self.nameRAW}> successfully")
+        if     self.convertFailed:    self.progress.append(f"Convert of <{self.nameRAW}> failed")
                
-    
-        # pro__print(f"convert done, succeed: {'Nope' if self.convertFailed else 'True'}, status: mesh={self.meshRcode} item={self.itemRcode}, obj: {self.name}")
         updateConvertStatusNumbers(result=not self.convertFailed, objname=self.name)
 
 
@@ -56,7 +60,7 @@ class CONVERT_ITEM(Thread):
 
     def convertMeshAndShapeGBX(self) -> None:
         """convert fbx to shape/mesh.gbx"""
-        self.progress.append("Start converting mesh/shape.gbx")
+        self.progress.append(f"""Convert <{self.nameRAW}.Mesh and Shape.gbx>""")
         
         cmd = f"{getNadeoImporterPath()} Mesh {self.fbxfilepathREL}" # ex: NadeoImporter.exe Mesh /Items/myblock.fbx
         convertProcess  = subprocess.Popen(cmd, stdout=subprocess.PIPE)
@@ -67,13 +71,16 @@ class CONVERT_ITEM(Thread):
         self.meshInfos = str(mesh_output[0])
         self.meshRcode = mesh_returncode
         self.convertFailed = True if mesh_returncode > 0 else False
+        self.progress.append(f"""Convert <{self.nameRAW}.Mesh and Shape.gbx> {"failed" if self.convertFailed else "success"}""")
+
     
     
     def convertItemGBX(self) -> None:
         """convert fbx to item.gbx"""
-        self.progress.append("Start converting item.gbx")
+        self.progress.append(f"Convert <{self.nameRAW}>.Item.gbx")
         
         cmd = f"{getNadeoImporterPath()} Item {self.itemXMLFilepathREL}" # ex: NadeoImporter.exe Mesh /Items/myblock.fbx
+        debug(cmd)
         convertProcess  = subprocess.Popen(cmd, stdout=subprocess.PIPE)
         item_output     = convertProcess.communicate()  # is blocking, => blender not frozen cuz this func runs in a new thread
         item_returncode = convertProcess.returncode
@@ -82,6 +89,8 @@ class CONVERT_ITEM(Thread):
         self.itemInfos = str(item_output[0])
         self.itemRcode = item_returncode
         self.convertFailed = True if item_returncode > 0 else False
+        self.progress.append(f"""Convert <{self.nameRAW}>.Item.gbx {"failed" if self.convertFailed else "success"}""")
+
     
     
     def hackShapeGBX(self, action: str="MAKE_OLD" or "USE_OLD") -> None:
@@ -94,7 +103,7 @@ class CONVERT_ITEM(Thread):
         #create shape.gbx.old file, should be used in first call
         if action == "MAKE_OLD":
             
-            self.progress.append("Start rename shape.gbx to shape.gbx.old")
+            self.progress.append(f"Hacking <{self.nameRAW}.Shape.gbx>, rename to <{self.nameRAW}.Shape.gbx.old>")
             
             if doesFileExist(self.shapeGBXFilepathOLD):
                 os.remove(self.shapeGBXFilepathOLD)
@@ -104,11 +113,13 @@ class CONVERT_ITEM(Thread):
                 self.shapeGBXFilepath,
                 self.shapeGBXFilepathOLD
                 )
+                self.progress.append(f"Hacking <{self.nameRAW}.Shape.gbx> success")
+
             except FileNotFoundError:
-                pass
+                self.progress.append(f"Hacking <{self.nameRAW}.Shape.gbx> failed, file not found")
             
             
-            self.progress.append("Start parsing item.xml")
+            self.progress.append(f"Parsing <{self.nameRAW}.Item.xml>")
             tree = ET.parse(self.meshparamsXMLFilepath)
             root = tree.getroot()
             data = root.findall(".Materials/Material")
@@ -123,17 +134,15 @@ class CONVERT_ITEM(Thread):
             # xmlstr = ET.tostring(root, encoding='utf8', method='xml')
             # pro__print(xmlstr)
                         
-            self.progress.append("Start writing changed xml to same file")
+            self.progress.append(f"Write changes to <{self.nameRAW}.Item.xml>")
             tree.write(self.meshparamsXMLFilepath)
-
-            debug("make shape")
 
 
         
         #replace shape.gbx with shape.old.gbx
         if action == "USE_OLD":
             
-            self.progress.append("Start replacing shape.gbx with shape.old.gbx")
+            self.progress.append(f"Hacking <{self.nameRAW}.Shape.gbx>, replace with <{self.nameRAW}.Shape.gbx.old>")
         
             try:    os.remove(self.shapeGBXFilepath)
             except: pass #doesn't work sometimes, for some unknown reason..
@@ -143,8 +152,10 @@ class CONVERT_ITEM(Thread):
                     self.shapeGBXFilepathOLD,
                     self.shapeGBXFilepath
                 )
+                self.progress.append(f"Hacking <{self.nameRAW}.Shape.gbx> success")
+
             except FileNotFoundError:
-                pass
+                self.progress.append(f"Hacking <{self.nameRAW}.Shape.gbx> failed, file not found")
         
         
 
@@ -173,32 +184,43 @@ def startBatchConvert(fbxfilepaths: list) -> None:
     """convert each fbx one after one, create a new thread for it"""
     tm_props = bpy.context.scene.tm_props
     results  = []
+    fails    = tm_props.NU_convertedError
+    report   = tm_props.CB_notifyPopupWhenDone
 
     tm_props.CB_showConvertPanel = True
+    game = "ManiaPlanet" if isGameTypeManiaPlanet() else "TrackMania2020"
 
     for fbx in fbxfilepaths:
-        convertTheFBX = CONVERT_ITEM(fbxfilepath=fbx[0])
+        convertTheFBX = CONVERT_ITEM(fbxfilepath=fbx[0], game=game)
         convertTheFBX.start() #start the convert (call internal run())
         convertTheFBX.join()  #waits until the thread terminated (function/convert is done..)
         
         results.append({
-            "name":         convertTheFBX.fbxfilepathREL,
+            "path":         convertTheFBX.fbxfilepathREL,
+            "nameRAW":      convertTheFBX.nameRAW,
             "meshRcode":    convertTheFBX.meshRcode,
             "itemRcode":    convertTheFBX.itemRcode,
             "itemInfos":    convertTheFBX.itemInfos,
             "meshInfos":    convertTheFBX.meshInfos,
             "progress":     convertTheFBX.progress
         })
-        debug("convert success: ", not convertTheFBX.convertFailed)
-        debug("mesh:", convertTheFBX.meshInfos)
-        debug("item:", convertTheFBX.itemInfos)
+        
+        for step in convertTheFBX.progress:
+            debug(step)
 
         if tm_props.CB_stopAllNextConverts is True:
-            debug("convert stopped, aborted by user (UI CHECKBOX)")
+            debug("Convert stopped, aborted by user (UI CHECKBOX)")
             break
 
     tm_props.CB_converting = False
     writeConvertReport(results=results)
+    
+    if report:
+        ...#not working yet -> appears in background...
+        # makeWindowsReportPopup("Converting done", [f"fails: {fails}"])
+
+
+
 
 
 def writeConvertReport(results: list) -> None:
@@ -206,55 +228,68 @@ def writeConvertReport(results: list) -> None:
     errors      = 0
     converted   = len(results)
     
-    for res in results:
-        if res["meshRcode"] > 0 or res["itemRcode"] > 0:
+    for result in results:
+        if result["meshRcode"] > 0 or result["itemRcode"] > 0:
             errors += 1
-    
-    with open(website_convertreport, "w", encoding="utf-8") as f:
-        
-        resultList = ""
-        for res in results:
+    try:
+        with open( fixSlash(PATH_CONVERT_REPORT), "w", encoding="utf-8") as f:
+            
+            resultList = ""
+            for result in results:
 
-            progressLIs = ""
-            for progress in res["progress"]:
-                progressLIs += f"""<li class="">{progress}</li> """
+                progressLIs = ""
+                for progress in result["progress"]:
+                    progress = progress.replace("<", "&lt;")
+                    progress = progress.replace(">", "&gt;")
+                    progressLIs += f"""<li class="">{progress}</li> """
 
-            resultList += f"""
-                <li class="{"error" if res["meshRcode"] > 0 or res["itemRcode"] > 0 else "success"}">
-                    <ul class="result-object">
-                        <li class="item"><b>Item:</b> {res["name"]}     </li>
-                        <li class="mesh-error"><b>Mesh Errors:</b> {beautifyError(res["meshInfos"]) if res["meshRcode"] > 0 else ""} </li>
-                        <li class="item-error"><b>Item Errors:</b> {beautifyError(res["itemInfos"]) if res["itemRcode"] > 0 else ""} </li>
-                        <li class="item-error"><b>Convert Progress Steps until failure:</b></li>
-                        <ul class="">
-                            {progressLIs}
-                        </ul>
-                    </ul>    
-                </li>
+                objName = result["path"]
+                objName = objName.replace(result["nameRAW"], f"""<i>{result["nameRAW"]}</i>""")
+
+                resultList += f"""
+                    <li class="{"error" if result["meshRcode"] > 0 or result["itemRcode"] > 0 else "success"}">
+                        <ul class="result-object">
+                            <li class="item"><b>Item:</b> {objName}  </li>
+                            <li class="mesh-error"><b>Mesh Errors:</b> {beautifyError(result["meshInfos"]) if result["meshRcode"] > 0 else ""} </li>
+                            <li class="item-error"><b>Item Errors:</b> {beautifyError(result["itemInfos"]) if result["itemRcode"] > 0 else ""} </li>
+                            <li class="item-error"><b>Convert steps until convert failed:</b></li>
+                            <ul class="progress-steps">
+                                {progressLIs}
+                            </ul>
+                        </ul>    
+                    </li>
+                    """
+            
+            fullHTML = f"""
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Convert Report</title>
+                    <link rel="stylesheet" href="file://{getAddonPath()}assets/report.css">
+                </head>
+                <body>
+                    <header>
+                        <div>
+                            Failed converts: {errors} of {converted}
+                        </div>
+                    </header>
+                    <ul id="result-list">
+                    {resultList}           
+                    </ul>
+                </body>
+                </html>
                 """
+            f.write(fullHTML)
         
-        fullHTML = f"""
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Document</title>
-                <link rel="stylesheet" href="file://{getAddonPath()}assets/report.css"
-            </head>
-            <body>
-                <header>
-                    <div>
-                        Failed converts: {errors} of {converted}
-                    </div>
-                </header>
-                <ul id="result-list">
-                {resultList}           
-                </ul>
-            </body>
-            </html>
-            """
-        f.write(fullHTML)
+    except FileNotFoundError as e:
+        makeReportPopup(
+            "Writing file failed", 
+            [
+                "Can not write report file on desktop",
+                "Try to run Blender once as admindistator" 
+            ])
 
 
         
