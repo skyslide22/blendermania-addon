521a524a01f17097e21b8ba2a253ce69b6972b37
skyslide <sir.von-niklas@hotmail.de>
2021-05-04 15:57:20 +0200

    fix lightmap gerations

--- TM_Items_UVMaps.py
+++ TM_Items_UVMaps.py
@@ -61,56 +61,72 @@ class TM_PT_Items_UVmaps_LightMap(Panel):
             col.row(align=True).prop(tm_props, "CB_uv_scaleToBoundsLM")
             
                     
-        layout.separator(factor=spacerFac)
+        layout.separator(factor=UI_SPACER_FACTOR)
 
 
 
 
 def generateLightmap(col, fix=False) -> None:
     """generate lightmap of all mesh objects from given collection"""
-    tm_props = bpy.context.scene.tm_props
-    objs     = col.all_objects
+    tm_props    = bpy.context.scene.tm_props
+    objs        = col.all_objects
+    lm_objs     = []
 
+    ANGLE = tm_props.NU_uv_angleLimitLM
+    MARGIN= tm_props.NU_uv_islandMarginLM
+    AREA  = tm_props.NU_uv_areaWeightLM
+    ASPECT= tm_props.CB_uv_correctAspectLM
+    BOUNDS= tm_props.CB_uv_scaleToBoundsLM
+
+    has_overlaps = False
+    only_if_olaps= tm_props.CB_uv_fixLightMap
+
+    deselectAll()
+
+    #select only objs which need a lightmap
     for obj in objs:
-        if selectObj(obj):
-            if obj.type == "MESH" and len(obj.material_slots.keys()) > 0:
-                if "lightmap" in [key.lower() for key in obj.data.uv_layers.keys()]:
+        if obj.type == "MESH" is False:
+            continue
 
-                    deselectAll()
-                    setActiveObj(obj)
+        obj_uvs = [k.lower() for k in obj.data.uv_layers.keys()]
 
-                    debug("check if lm has overlaps")
-                    isBrokenLM = hasUVLayerOverlaps(obj, "LightMap") 
-                    debug("overlaps:", isBrokenLM)
-                    
-                    if fix and not isBrokenLM: continue
-                    
-                    obj.data.uv_layers.active_index = 1
-                    editmode()
-                    
-                    ANGLE = tm_props.NU_uv_angleLimitLM
-                    MARGIN= tm_props.NU_uv_islandMarginLM
-                    AREA  = tm_props.NU_uv_areaWeightLM
-                    ASPECT= tm_props.CB_uv_correctAspectLM
-                    BOUNDS= tm_props.CB_uv_scaleToBoundsLM
+        if   "trigger" not in obj.name.lower()\
+        and  "socket"  not in obj.name.lower()\
+        and  obj.name.startswith("_") is False\
+        and  "lightmap" in obj_uvs:
+            selectObj(obj)
+            setActiveObj(obj)
+            lm_objs.append(obj)
 
-                    debug(f"create lm for {obj.name}")
-                    bpy.ops.mesh.select_all(action='SELECT')
+            obj.data.uv_layers.active_index = 1 # 0=BaseMaterial; 1=LightMap
+            
+            if hasUVLayerOverlaps(obj, "LightMap"): 
+                has_overlaps: True
+            
 
-                    bpy.ops.uv.smart_project(
-                        angle_limit     = ANGLE, 
-                        island_margin   = MARGIN,
-                        area_weight     = AREA,
-                        correct_aspect  = ASPECT,
-                        scale_to_bounds = BOUNDS
-                    )
+    if lm_objs:
+        if  only_if_olaps\
+        and has_overlaps\
+        or  only_if_olaps is False:
+            #editmode with all selected objects
+            editmode()
+            bpy.ops.mesh.select_all(action='SELECT')
+            bpy.ops.uv.smart_project(
+                angle_limit     = ANGLE, 
+                island_margin   = MARGIN,
+                area_weight     = AREA,
+                correct_aspect  = ASPECT,
+                scale_to_bounds = BOUNDS
+            )
+            objectmode()
+    
+    for obj in objs:
+        obj_uvs = [k.lower() for k in obj.data.uv_layers.keys()]
 
-                    debug("lm creation successfully")
+        if "basematerial" in obj_uvs:
+            obj.data.uv_layers.active_index = 0 #BaseMaterial
 
-                    objectmode()
-
-                    if "BaseMaterial" in obj.data.uv_layers.keys():
-                        obj.data.uv_layers["BaseMaterial"].active = True
+    
                         
 
 
