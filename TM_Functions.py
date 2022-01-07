eae3b0732c45651ed61160a451e9484d9d0e6e65
Vladimir <froladwork@gmail.com>
2022-01-06 23:53:48 -0800

    check

--- TM_Functions.py
+++ TM_Functions.py
@@ -36,8 +36,8 @@ def getDocumentsPath() -> str:
         SHGFP_TYPE_CURRENT = 0  # Get current, not default value
 
         buf = ctypes.create_unicode_buffer(ctypes.wintypes.MAX_PATH)
-        ctypes.windll.shell32.SHGetFolderPathW(None, CSIDL_PERSONAL, None, SHGFP_TYPE_CURRENT, buf)
-        documentsPath = buf.value
+        if ctypes.windll.shell32.SHGetFolderPathW(None, CSIDL_PERSONAL, None, SHGFP_TYPE_CURRENT, buf) == 0:
+            documentsPath = buf.value
 
     return documentsPath.replace("\\", "/")
 
