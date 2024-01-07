0a0cd7a4916cccae0a71ca695c7f2e9aeb223161
Alexis Schad <schad.alexis@gmail.com>
2024-01-07 11:56:58 +0100

    put bl_info back in init_py + read it from release script with ast

--- release.py
+++ release.py
@@ -3,8 +3,7 @@ import os
 import subprocess
 import shutil
 import glob
-
-from bl_info import bl_info
+import ast
 
 RELEASE_WORK_DIR = "tmp_release/"
 
@@ -29,7 +28,19 @@ PATTERNS_TO_RELEASE = [
     "NICE/**/__init__.py",
 ]
 
+def get_bl_info():
+    with open("__init__.py", "r", encoding='UTF-8') as init_file:
+        data = init_file.read()
+        ast_data = ast.parse(data)
+        for body in ast_data.body:
+            if body.__class__ == ast.Assign:
+                if len(body.targets) == 1:
+                    if getattr(body.targets[0], "id", "") == "bl_info":
+                        return ast.literal_eval(body.value)
+
 def get_release_filename():
+    bl_info = get_bl_info()
+
     bmv_major, bmv_minor, bmv_patch = bl_info["version"]
     blv_major, blv_minor, _ = bl_info["blender"]
 
@@ -38,10 +49,15 @@ def get_release_filename():
 
 
 def make_release_zip():
-    # Ensure work dir is clean
+    release_filename = get_release_filename()
+
+    # Ensure work dir & export file are clean
 
     if os.path.exists(RELEASE_WORK_DIR):
         shutil.rmtree(RELEASE_WORK_DIR)
+
+    if os.path.exists(release_filename):
+        os.remove(release_filename)
 
     # clone the repo
 
@@ -56,11 +72,8 @@ def make_release_zip():
 
     # generate the zip with the whitelisted files
 
-    release_filename = get_release_filename()
-
     with zipfile.ZipFile(release_filename, 'w') as f:
         for pattern in PATTERNS_TO_RELEASE:
-            print(RELEASE_WORK_DIR + pattern)
             for file in glob.glob(RELEASE_WORK_DIR + pattern, recursive=True):
                 f.write(file, os.path.relpath(file, RELEASE_WORK_DIR))
 
