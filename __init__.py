246e0fa69a427719f9ae33f04864df3398608753
Vladimir Frolov <frolad@gmail.com>
2024-04-16 21:49:08 -0700

    4.0.5

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@ bl_info = {
     "author"        : "skyslide22, juice & schadocalex",
     "description"   : "Makes creating objects for Trackmania more easy and fun!",
     "blender"       : (4, 0, 0),
-    "version"       : (4, 0, 4),
+    "version"       : (4, 0, 5),
     "location"      : "View3D",
     "warning"       : "",
     "category"      : "Generic",
